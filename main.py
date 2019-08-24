f08fb86a547194fa4f0a34b4e605d8569d7b0098
afuentes@skytouchtechnology.com <afuentes@skytouchtechnology.com>
2019-08-24 12:16:09 -0500

    Primer propeusta de algoritmo de dithering

---
+++ main.py
@@ -0,0 +1,22 @@
+from skimage import io
+from skimage import color
+import numpy as np
+import dithering as dt
+import matplotlib.pyplot as plt
+
+Img = io.imread("lena512color.tiff")
+Dithered = dt.dither_image(Img)
+
+fig = plt.figure("RGB Dithering")
+ 
+ax = fig.add_subplot(1, 2, 1)
+plt.imshow(Img)
+plt.axis("off")
+ 
+ax = fig.add_subplot(1, 2, 2)
+plt.imshow(Dithered)
+plt.axis("off")
+
+
+plt.show()
+        
